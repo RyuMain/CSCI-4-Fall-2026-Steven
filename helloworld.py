5668450ec7cb82c9a793448a3c61d0c03db9fbd2
$teven <slindsay003@student.butte.edu>
2026-08-27 22:58:22 +0000

    commit lab 3

---
+++ helloworld.py
@@ -0,0 +1,14 @@
+import numpy
+
+listofcolors = ["red", "blue", "green"]
+colors = numpy.array(listofcolors)
+
+print (colors)
+
+listofcolors = {"red", "blue", "green"}
+mypets = {"jessie", "lindo", "skye"}
+lilguys = {"hampter", "red pandas", "chipmunks"}
+
+print(listofcolors)
+print(mypets)
+print(lilguys)

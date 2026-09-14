cca13cc3ae15e9dd9d7834bc24a5bff09e5251f4
$teven <slindsay003@student.butte.edu>
2026-09-14 02:14:49 +0000

    rubiks_cube_lab

---
+++ cube_matrix.py
@@ -0,0 +1,12 @@
+cube_face_1 = [0,0,0,0,0,0,0,0,0]
+ 
+cublet_row = [0,0,0]
+
+cube_matrix = [
+ [0,0,0],
+ [0,0,0],
+ cublet_row
+]
+
+print(cube_matrix)
+ #im attempting to build a rubik's cube in the future
